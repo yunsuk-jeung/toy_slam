eac9bc0ca2e6d8ca3e0f31f0c51dcfa51902d9f1
ysjeung <yunsukjeung@snu.ac.kr>
2023-11-05 22:39:37 +0900

    eigen tbb

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <tbb/concurrent_unordered_map.h>
+#include <Eigen/Dense>
 
-int main(){
-  for(int i =0; i < 100; i++){
-  std::cout << "Hello c++" << std::endl;
+using Map = tbb::concurrent_unordered_map<int, int, std::hash<int>>;
+
+int main() {
+
+  Map map;
+
+  int i = 0;
+
+  map.emplace(i, i);
+  i++;
+  map.emplace(i, i);
+  i++;
+  map.emplace(i, i);
+  i++;
+  map.emplace(i, i);
+  map.emplace(i, i);
+  map.emplace(i, i);
+  map.emplace(i, i);
+
+  Eigen::Matrix4f I = Eigen::Matrix4f::Identity();
+  std::cout << I << std::endl;
+
+  for (auto aa = map.begin(); aa != map.end(); aa++) {
+    std::cout << aa->first << " / " << aa->second << std::endl;
+  }
+
+  for(auto& [key,val] : map ){
+    std::cout << key << " / " << val << std::endl;
   }
 
   return 0;
